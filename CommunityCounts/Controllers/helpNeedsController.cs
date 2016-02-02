e3b9873757b56ede7ac35d0da937e062acd656d9
Greg Neumann <greg@nsfwd.co.uk>
2016-02-02 14:47:00 +0000

    Added objects missed from commint since Client Needs process created - LOL!

---
+++ CommunityCounts/Controllers/helpNeedsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace CommunityCounts.Controllers
+{
+    [Authorize]
+    public class helpNeedsController : Controller
+    {
+        // GET: helpSurvey
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
