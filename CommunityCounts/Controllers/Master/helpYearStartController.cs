84a6f2e4ca9d2e82fddd70d5a5f437afa48f35aa
Greg Neumann <greg@nsfwd.co.uk>
2016-02-28 13:07:28 +0000

    Formatting on Start Year Help Text PLUS added new DIv to Footer to ensure that it always floated to bottom left of page

---
+++ CommunityCounts/Controllers/Master/helpYearStartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityCounts.Controllers.Master
+{
+    public class helpYearStartController : Controller
+    {
+        // GET: helpYearStart
+        public ActionResult StartYear()
+        {
+            return View();
+        }
+    }
+}
